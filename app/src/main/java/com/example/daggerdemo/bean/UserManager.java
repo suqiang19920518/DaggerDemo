2138ccaaff32e7dc977201047b4163b03e591180
suqiang <1466535694@qq.com>
2023-07-22 19:24:23 +0800

    update

--- app/src/main/java/com/example/daggerdemo/bean/UserManager.java
+++ app/src/main/java/com/example/daggerdemo/bean/UserManager.java
@@ -12,7 +12,7 @@ public class UserManager {
     private final Storage storage;
 
     // @Inject 告诉Dagger如何提供 UserManager 对象
-    // 由于 UserManager负责管理UserComponent的生命周期，它需要知道如何创建该对象，因此将factory传入
+    // 由于 UserManager负责管理Storage的生命周期，它需要知道如何创建该对象，因此将Storage传入
     @Inject
     public UserManager(Storage storage) {
         this.storage = storage;
--- app/src/main/java/com/example/daggerdemo/setting/UserInfo.java
+++ app/src/main/java/com/example/daggerdemo/setting/UserInfo.java
@@ -10,6 +10,7 @@ public class UserInfo {
     private UserComponent userComponent;
     private final UserComponent.Factory userComponentFactory;
 
+    // 由于 UserInfo负责管理UserComponent的生命周期，它需要知道如何创建该对象，因此将factory传入
     @Inject
     public UserInfo(UserComponent.Factory userComponentFactory) {
         this.userComponentFactory = userComponentFactory;
